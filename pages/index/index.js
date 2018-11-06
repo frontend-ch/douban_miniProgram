561fe3b2cfb5ef7ce6529a17c4b47e5e60ac34f7
李钰 <yu6.li@changhong.com>
2018-11-06 17:52:46 +0800

    排行

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,6 +4,8 @@ const app = getApp()
 Page({
   data: {
     near_movies:[],
+    hot_movies:[],
+    top_movies: [],
     url:''
   },
   //事件处理函数
@@ -13,6 +15,8 @@ Page({
   onLoad: function () {
     var that=this
     that.loadMovie()
+    that.loadHotMovie()
+    that.loadTopMovie()
   },
   
   loadMovie:function(){
@@ -26,6 +30,30 @@ Page({
     (err)=>{
       console.log('err')
     })
+  },
+  loadHotMovie: function () {
+    var server = 'in_theaters'
+    app.http(server, {}, (res) => {
+      console.log(res)
+      this.setData({
+       hot_movies: res
+      })
+    },
+      (err) => {
+        console.log('err')
+      })
+  },
+  loadTopMovie:function(){
+    var server = 'top250?start=0&count=20'
+    app.http(server, {}, (res) => {
+      console.log(res)
+      this.setData({
+        top_movies: res
+      })
+    },
+      (err) => {
+        console.log('err')
+      })
   },
   storageMovie:function(subjects){
     for(let i=0;i<subjects.length;i++){
